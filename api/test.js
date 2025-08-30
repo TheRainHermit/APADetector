3d7cbff3f4b788ad2ac1e1a060678184a15c16e0
TheRainHermit <miguel.fabra211294@gmail.com>
2025-08-30 03:01:11 -0500

    Despliegue 39: Teoría del descarte.

--- api/test.js
+++ api/test.js
@@ -1,12 +1,3 @@
-import express from 'express';
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.json({ ok: true, info: 'API analyze GET works' });
-});
-
-// Este export es el recomendado:
 export default function handler(req, res) {
-  app(req, res);
+  res.json({ ok: true, info: 'Direct handler test works' });
 }
